3b7dc9e6bf1db40646e6fc1f806d63a6c483798c
Connor Tumbleson <connor@sourcetoad.com>
2024-02-08 09:57:17 -0500

    build: add basic prettier

---
+++ packages/prettier/index.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  bracketSameLine: false,
+  singleAttributePerLine: false,
+};
+
+export default config;
