7743af996cb4141aa3e8ccd0b55f37bd6d06c253
Connor Tumbleson <connor@sourcetoad.com>
2024-08-15 08:50:05 -0400

    chore: further progress to flat configs

--- packages/vue/eslint.config.js
+++ packages/vue/eslint.config.js
@@ -1,34 +1,37 @@
-// TODO https://eslint.org/docs/latest/use/configure/configuration-files-new
-export default [{
-  'extends': [
-    '@sourcetoad/eslint-config-base-template-es6',
-    'plugin:vue/vue3-essential',
-  ],
-  'plugins': [
-    'vue',
-    'unused-imports',
-  ],
-  'rules': {
-    'vue/multi-word-component-names': ['off'],
-    'vue/html-indent': [
-      'error',
-      4,
-      {
-        attribute: 1,
-        baseIndent: 1,
-        closeBracket: 0,
-        alignAttributesVertically: true,
-        ignores: [],
-      },
-    ],
-    'vue/script-indent': [
-      'error',
-      4,
-      {
-        baseIndent: 0,
-        switchCase: 1,
-        ignores: [],
-      },
-    ],
-  }
-}];
+import unusedImports from 'eslint-plugin-unused-imports';
+import pluginVue from 'eslint-plugin-vue';
+import baseTemplateConfigEs6 from '../es6/eslint.config.js';
+
+export default [
+    ...pluginVue.configs['flat/essential'],
+    ...baseTemplateConfigEs6,
+    {
+        name: 'base-template-config-vue',
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+        rules: {
+            'vue/multi-word-component-names': ['off'],
+            'vue/html-indent': [
+                'error',
+                4,
+                {
+                    attribute: 1,
+                    baseIndent: 1,
+                    closeBracket: 0,
+                    alignAttributesVertically: true,
+                    ignores: [],
+                },
+            ],
+            'vue/script-indent': [
+                'error',
+                4,
+                {
+                    baseIndent: 0,
+                    switchCase: 1,
+                    ignores: [],
+                },
+            ],
+        },
+    },
+];
