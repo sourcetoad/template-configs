b4391a6125e5794a34a7196a0a8153557733902a
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-08-19 08:30:15 -0400

    Flat Configs (#11)
    * feat: flat configs
    
    * chore: upgrade to eslint v9
    
    * build: peek to typescript-eslint
    
    * feat: add lint:fix to vue
    
    * chore: further progress to flat configs
    
    * feat: basic eslint.config.js for prettier configs
    
    * chore: migrate "env" to globals
    
    * fix: reference to es6 (from vue)
    
    * fix: add typescript parser
    
    * fix: move vue package to vue parser
    
    * chore: move back to v0.1.1
    
    * chore: update packages/es6/eslint.config.js
    
    Co-authored-by: Erik Perri <46399654+erik-perri@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Erik Perri <46399654+erik-perri@users.noreply.github.com>

---
+++ packages/es6/eslint.config.js
@@ -0,0 +1,46 @@
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import {configs, parser, plugin} from 'typescript-eslint';
+
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
+export default [
+    ...configs.recommended,
+    {
+        name: 'base-template-config-es6',
+        plugins: {
+            '@typescript-eslint': plugin,
+            'simple-import-sort': eslintPluginSimpleImportSort,
+        },
+        languageOptions: {
+            parser: parser,
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
+        rules: {
+            'array-bracket-spacing': ['error', 'never'],
+            'comma-dangle': ['error', 'always-multiline'],
+            'eqeqeq': 'error',
+            'indent': ['error', 4, {'SwitchCase': 1}],
+            'max-len': ['error', 120, {'ignorePattern': '^import'}],
+            'no-console': ['error', {'allow': ['warn', 'error']}],
+            'object-curly-spacing': ['error', 'never'],
+            'prefer-const': 'warn',
+            'quotes': [
+                'error',
+                'single',
+                {
+                    'avoidEscape': true,
+                },
+            ],
+            'semi': ['error'],
+            'simple-import-sort/imports': [
+                'error',
+                {
+                    'groups': [['^\\u0000', '^node:', '^@?\\w', '^', '^\\.']],
+                },
+            ],
+        },
+    },
+];
---
+++ packages/vue/eslint.config.js
@@ -0,0 +1,43 @@
+import unusedImports from 'eslint-plugin-unused-imports';
+import pluginVue from 'eslint-plugin-vue';
+import baseTemplateConfigEs6 from '../es6/eslint.config.js';
+
+export default [
+    ...pluginVue.configs['flat/essential'],
+    ...baseTemplateConfigEs6,
+    {
+        name: 'base-template-config-vue',
+        plugins: {
+            'vue': pluginVue,
+            'unused-imports': unusedImports,
+        },
+        languageOptions: {
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+            },
+        },
+        rules: {
+            'vue/multi-word-component-names': ['off'],
+            'vue/html-indent': [
+                'error',
+                4,
+                {
+                    attribute: 1,
+                    baseIndent: 1,
+                    closeBracket: 0,
+                    alignAttributesVertically: true,
+                    ignores: [],
+                },
+            ],
+            'vue/script-indent': [
+                'error',
+                4,
+                {
+                    baseIndent: 0,
+                    switchCase: 1,
+                    ignores: [],
+                },
+            ],
+        },
+    },
+];
